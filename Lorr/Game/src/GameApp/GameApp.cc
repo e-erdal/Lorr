119e219d1119357250fc44293e95c7c2c0539f7d
e-erdal <loanselot@hotmail.com>
2021-08-15 19:00:43 +0300

    File write stream

--- Lorr/Game/src/GameApp/GameApp.cc
+++ Lorr/Game/src/GameApp/GameApp.cc
@@ -1,9 +1,21 @@
 #include "GameApp.hh"
 
-#include "Core/Stream/BufferStream.hh"
+#include "Core/Stream/FileStream.hh"
 
 void GameApp::Init()
 {
+    using namespace Lorr;
+
+    FileStream file( "test.txt", true );
+
+    file.WriteString( "Holy shit\n" );
+    file.WriteString( "Holy shit" );
+    file.WriteString( "Holy shit" );
+    file.WriteString( "Holy shit" );
+    file.WriteString( "Holy shit" );
+    file.WriteString( "Holy shit" );
+
+    file.Close();
 }
 
 void GameApp::Tick( float fDelta )
