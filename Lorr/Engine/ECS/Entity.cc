2551045b1166a0dbb80246a9e2be5a9d55475b50
e-erdal <loanselot@hotmail.com>
2021-12-07 00:49:24 +0300

    Add Tracy Scope to missing functions

--- Lorr/Engine/ECS/Entity.cc
+++ Lorr/Engine/ECS/Entity.cc
@@ -6,12 +6,15 @@ namespace Lorr
 {
     Entity::Entity(const Identifier &ident, EntityRegistry *pRegistry) : m_Registry(pRegistry)
     {
+        ZoneScoped;
+
         m_Handle = pRegistry->create();
         AddComponent<Component::Base>(ident);
     }
 
     Entity::Entity(EntityRegistry *pRegistry, const EntityHandle &other) : m_Registry(pRegistry), m_Handle(other)
     {
+        ZoneScoped;
     }
 
 }  // namespace Lorr
